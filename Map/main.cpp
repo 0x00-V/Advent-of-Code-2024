c1deebb7d1520693142d30cf32134db51f77eb2e
0x00-V <93012418+0x00-V@users.noreply.github.com>
2024-12-08 01:55:17 +0000

    Unfinished

---
+++ Map/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+// if . move, else if # turn 90 move
+
+// for 90 deg, use diff dir symbol, move along facing vertcies
+
+void mapToGrid(std::ifstream& file, std::vector<std::vector<char>>& grid)
+{
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::vector<char> row(line.begin(), line.end());
+        grid.push_back(row);
+    }
+}
+
+
+int main()
+{
+    std::ifstream file("input.txt");
+    if(!file){
+        std::cerr << "Couldn't find file" << std::endl;
+        return 1;
+    }
+
+    std::vector<std::vector<char>> map;
+    mapToGrid(file, map);
+
+    int up{1};
+    for(size_t row = 0; row < map.size(); ++row)
+    {
+        for(size_t col = 0; col < map[row].size(); ++col)
+        {
+            if(map[row][col] == '^')
+            {
+                std::cout << "Found Guard(^) at pos: (" << row << col << ")\n";
+                std::cout << map[row-1][col-1] << map[row-1][col] << map[row-1][col+1] << '\n' << map[row][col-1] << map[row][col] << map[row][col+1] << '\n' << map[row+1][col-1] << map[row+1][col] << map[row+1][col+1];
+
+
+                // ^ means check  row - until # is detected.
+                while (map[row-up][col] != '#')
+                {
+                    std::cout << map[row-up+1][col] << '\n'; // collision detected
+                    --up;
+                }
+            }
+
+            
+            
+            
+        }
+
+    }
+    
+
+
+
+    return 0;
+}
